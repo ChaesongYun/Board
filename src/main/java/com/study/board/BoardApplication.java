9e4c1694cd54e2d8b4d4f9a780ad052f2a9bbca3
ChaesongYun <hapbbie@naver.com>
2024-04-17 20:16:06 +0900

    세 번째 커밋

--- src/main/java/com/study/board/BoardApplication.java
+++ src/main/java/com/study/board/BoardApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardApplication {
 
 	public static void main(String[] args) {
-		// 깃 커밋 테스트3
+		// 깃 커밋 테스트333
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
